c51906689fa3e4911f3e637a368fde7ee62c5545
maneesha <ahseenam@gmail.com>
2020-12-23 12:38:13 -0500

    add sample data files

---
+++ data/places.js
@@ -0,0 +1,115 @@
+// see possible icons here: https://github.com/mapbox/mapbox-gl-styles
+
+var places = {
+'type': 'FeatureCollection',
+'features': [
+//--------------------------
+{
+'type': 'Feature',
+'properties': {
+'icon': 'theatre',
+'condition':'new',
+'txtlab':"one",
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [-77.038659, 38.931567]
+}
+},
+//--------------------------
+{
+'type': 'Feature',
+'properties': {
+'icon': 'theatre',
+'condition':'medium', 
+'txtlab':"two",
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [-77.003168, 38.894651]
+}
+},
+//--------------------------
+{
+'type': 'Feature',
+'properties': {
+'icon': 'bar',
+'condition':'poor',
+'txtlab':"three",
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [-77.090372, 38.881189]
+}
+},
+//--------------------------
+{
+'type': 'Feature',
+'properties': {
+'icon': 'bank',
+'condition':'gone',
+'txtlab':"four",
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [-77.052477, 38.943951]
+}
+},
+//--------------------------
+{
+'type': 'Feature',
+'properties': {
+'icon': 'music',
+'condition':'new',
+'txtlab':"five",
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [-77.031706, 38.914581]
+}
+},
+//--------------------------
+{
+'type': 'Feature',
+'properties': {
+'icon': 'music',
+'condition':'gone',
+'txtlab':"six",
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [-77.020945, 38.878241]
+}
+},
+//--------------------------
+{
+'type': 'Feature',
+'properties': {
+'icon': 'music',
+'condition':'medium',
+'txtlab':"seven",
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [-77.007481, 38.876516]
+}
+},
+//--------------------------
+{
+'type': 'Feature',
+'properties': {
+'icon': 'toilet',
+'condition':'gone',
+'txtlab':"eight",
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [-77.027481, 38.876516]
+}
+},
+
+//--------------------------
+
+
+]
+};
